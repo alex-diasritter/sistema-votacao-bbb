88c04a911bb73bf8cc68b182b1b67e69940665df
alex-diasritter <ritter.alex@hotmail.com>
2025-08-29 13:42:37 -0300

    feat: dto para receber votos

---
+++ src/main/java/com/alex/desafio_bbb_votar/dto/VotoDto.java
@@ -0,0 +1,21 @@
+package com.alex.desafio_bbb_votar.dto;
+
+public class VotoDto {
+
+    private int voto;
+
+    public VotoDto() {
+    }
+
+    public VotoDto(int voto) {
+        this.voto = voto;
+    }
+
+    public int getVoto() {
+        return voto;
+    }
+
+    public void setVoto(int voto) {
+        this.voto = voto;
+    }
+}
