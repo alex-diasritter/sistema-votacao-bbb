40c48e15dfc4ad416fb11e95a2ef9fe689479a9d
alex-diasritter <ritter.alex@hotmail.com>
2025-08-29 13:40:43 -0300

    feat: estrutura inicial do projeto

---
+++ src/main/java/com/alex/desafio_bbb_votar/DesafioBbbApplication.java
@@ -0,0 +1,13 @@
+package com.alex.desafio_bbb_votar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBbbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBbbApplication.class, args);
+	}
+
+}
